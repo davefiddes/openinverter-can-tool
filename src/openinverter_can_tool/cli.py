2c45c1e765b08aac4fd09dd2890af06b5a9619c6
David J. Fiddes <D.J@fiddes.net>
2022-11-25 18:39:07 +0000

    Dump all parameters over CAN
    This adds a "dumpall" command that fetches and displays all
    the values on the requested node.
    
    Communicating over CAN requires several new options to
    configure the CAN bus. These all have sensible defaults and
    are stored in a custom CliSetting class that can be used to
    decorate new commands like "dumpall".
    
    The print_parameters function is rolled into the listparams
    command as it's very simple.
    
    Tests:
     - Verify operation of the new command against an STM32
        Bluepill openinverter succeeds
     - Try incorrect node numbers, can channel and speeds and
        verify they don't work
     - Verify parameters against web interface
     - Check command help makes sense

--- src/openinverter_can_tool/cli.py
+++ src/openinverter_can_tool/cli.py
@@ -3,14 +3,74 @@ openinverter CAN Tools main program
 """
 
 import click
-from canopen import objectdictionary
+import canopen
 from .paramdb import import_database
 from .fpfloat import fixed_to_float
 
 
-def print_parameters(database: objectdictionary.ObjectDictionary) -> None:
-    """Print out the currently loaded parameter database"""
-    for item in database.names.values():
+class CliSettings:
+    """Simple class to store the common settings used for all commands"""
+
+    def __init__(
+        self,
+        database: canopen.ObjectDictionary,
+            bustype: str,
+            channel: str,
+            speed: int,
+            node_number: int) -> None:
+        self.database = database
+        self.bustype = bustype
+        self.channel = channel
+        self.speed = speed
+        self.node_number = node_number
+
+
+pass_cli_settings = click.make_pass_decorator(CliSettings)
+
+
+@click.group()
+@click.option("-d", "--database",
+              help="openinverter JSON parameter database to use",
+              type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.option("-b", "--bustype",
+              default="socketcan",
+              show_default=True,
+              type=click.STRING,
+              help="Which python-can bus type to use")
+@click.option("-c", "--channel",
+              default="can0",
+              show_default=True,
+              type=click.STRING,
+              help="Which python-can bus channel to use")
+@click.option("-s", "--speed",
+              default=500000,
+              show_default=True,
+              type=click.INT,
+              help="The CAN bus speed. Typically 500kbps for openinverter "
+              "boards")
+@click.option("-n", "--node",
+              default=1,
+              show_default=True,
+              type=click.INT,
+              help="The CAN SDO node ID to communicate with")
+@click.version_option()
+@click.pass_context
+def cli(ctx, database, bustype, channel, speed, node):
+    """openinverter CAN Tool allows querying and setting configuration of
+    inverter parameters over a CAN connection"""
+    if not database:
+        raise click.BadParameter(
+            'Parameter database filename not given', param_hint='-d')
+
+    ctx.obj = CliSettings(import_database(database),
+                          bustype, channel, speed, node)
+
+
+@cli.command()
+@pass_cli_settings
+def listparams(cli_settings: CliSettings):
+    """List all available parameters and values"""
+    for item in cli_settings.database.names.values():
         print(
             f"{item.name} [{item.unit}]", end='')
 
@@ -23,28 +83,31 @@ def print_parameters(database: objectdictionary.ObjectDictionary) -> None:
             print(" - read-only value")
 
 
-@click.group()
-@click.option("-d", "--database",
-              help="openinverter JSON parameter database to use",
-              type=click.Path(exists=True, file_okay=True, dir_okay=False))
-@click.version_option()
-@click.pass_context
-def cli(ctx, database):
-    """openinverter CAN Tool allows querying and setting configuration of
-    inverter parameters over a CAN connection"""
-    if not database:
-        raise click.BadParameter(
-            'Parameter database filename not given', param_hint='-d')
-
-    # ensure that ctx.obj exists and is a dict (in case `cli()` is called
-    # by means other than the `if` block below)
-    ctx.ensure_object(dict)
-
-    ctx.obj['DATABASE'] = import_database(database)
-
-
 @cli.command()
-@click.pass_context
-def listparams(ctx):
-    """List all available parameters and values"""
-    print_parameters(ctx.obj['DATABASE'])
+@pass_cli_settings
+def dumpall(cli_settings: CliSettings):
+    """Dump the values of all available parameters and values"""
+
+    try:
+        # Start with creating a network representing one CAN bus
+        network = canopen.Network()
+
+        # Connect to the CAN bus
+        network.connect(
+            bustype=cli_settings.bustype,
+            channel=cli_settings.channel,
+            bitrate=cli_settings.speed)
+
+        network.check()
+
+        node = canopen.BaseNode402(
+            cli_settings.node_number, cli_settings.database)
+        network.add_node(node)
+
+        for item in cli_settings.database.names.values():
+            click.echo(
+                f"{item.name:20}: {fixed_to_float(node.sdo[item.name].raw):g}")
+
+    finally:
+        if network:
+            network.disconnect()
