f719b211e7a2788618c83089a978e809facf5e04
David J. Fiddes <D.J@fiddes.net>
2024-06-22 11:36:26 +0100

    Test loading Zombieverter VCU database
    This tests the database download from a Zombieverter
    VCU running 2.17.A firmware in an attempt to replicate
    issue #4.
    
    Tests:
     - Verify the test fails currently due to bugs in handling
        the database format.

--- tests/test_remote_db.py
+++ tests/test_remote_db.py
@@ -1,13 +1,19 @@
 """openinverter remote database unit tests"""
+import csv
+import tempfile
 import unittest
+from pathlib import Path
 from typing import List, Tuple
 
 import canopen
 
+from openinverter_can_tool.paramdb import import_database
 from openinverter_can_tool.remote_db import RemoteDatabaseNode
 
 TX = 1
 RX = 2
+
+CAPTURE_DATA_DIR = Path(__file__).parent / "test_data" / "captures"
 
 # Reduce test verbosity
 # pylint: disable=missing-function-docstring
@@ -55,6 +61,46 @@ class TestRemoteDatabaseNode(unittest.TestCase):
         ]
         checksum = self.node.param_db_checksum()
         assert checksum == 94226
+
+    def load_capture(self, capture: str, tx_id: int, rx_id: int) -> None:
+        """Load a CAN capture from SavvyCAN in CSV format into the data"""
+        with open(CAPTURE_DATA_DIR / capture,
+                  newline="", encoding="utf-8") as capture_file:
+            reader = csv.DictReader(capture_file)
+
+            for row in reader:
+                frame_id = int(row["ID"], 16)
+
+                if frame_id == tx_id:
+                    direction = TX
+                elif frame_id == rx_id:
+                    direction = RX
+                else:
+                    continue
+
+                assert int(row["LEN"]) == 8
+
+                frame = row["D1"] + row["D2"] + row["D3"] + row["D4"] + \
+                    row["D5"] + row["D6"] + row["D7"] + row["D8"]
+                self.data.append((direction, bytes.fromhex(frame)))
+
+    def test_query_zombieverter_paramdb(self):
+        self.load_capture(
+            "zombieverter-node3-query-paramdb.csv",
+            0x603,
+            0x583)
+        self.node.node_id = 3
+
+        checksum = self.node.param_db_checksum()
+
+        with tempfile.NamedTemporaryFile(delete_on_close=False) as db_file:
+            db_file.write(self.node.param_db())
+            db_file.close()
+
+            db = import_database(Path(db_file.name))
+
+        assert checksum == 181129
+        assert len(db) == 195
 
 
 if __name__ == "__main__":
