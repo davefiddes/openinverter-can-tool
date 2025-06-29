9c1ff64d633ae2296c2c45d01768bf673a62b33a
David J. Fiddes <D.J@fiddes.net>
2025-06-29 17:24:23 +0100

    Update to 0.3.2 for a maintenance release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = re.sub(r'\[!\[Build status.*\)\n\n', '', long_description)
 
 setup(
     name="openinverter-can-tool",
-    version="0.3.1",
+    version="0.3.2",
     description="Tool to configure and operate openinverter systems over CAN",
     long_description=long_description,
     long_description_content_type="text/markdown",
